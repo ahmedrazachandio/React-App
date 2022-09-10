c161bfa15a0eb11618b751010a88d29b4736dc83
Ali Raza Chandio <86877851+ahmedrazachandio@users.noreply.github.com>
2022-09-10 17:24:52 +0500

    navbar push

--- digital-clock/src/App.js
+++ digital-clock/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App() {
+// Navbar.js
+export default function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        MacroSoft
+      </a>
+      <button className="hamburger" 
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
-
-export default App;
