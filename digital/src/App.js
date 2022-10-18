00b510c56c320ffb19b7e0b6b5c6d97fe7500897
AhmadRaza <ahmadraza.azt@gmail.com>
2022-10-19 00:09:11 +0500

    code push

--- digital/src/App.js
+++ digital/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, seCtime] = useState(time);
+
+  const UpdateTime = () =>{
+    time = new Date().toLocaleTimeString();
+    seCtime(time);
+  }
+  setInterval(UpdateTime , 1000);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="time">{ctime}</h1>
     </div>
   );
 }
